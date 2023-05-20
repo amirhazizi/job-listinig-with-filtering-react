4de50604218f936e68ce9311864295355465f6db
amirhazizi <amirazizihosein@gmail.com>
2023-05-20 23:59:30 +0330

    setup pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,4 @@
 import "./globals.css"
-import { League_Spartan } from "next/font/google"
-
-// const leagueSpartan = League_Spartan({
-//   weight: ["500", "700"],
-//   subsets: ["latin"],
-// })
-
-export const metadata = {
-  title: "Frontend Mentor | Job Listings",
-  description: "Frontend Mentor | Job Listings",
-}
-
 export default function RootLayout({
   children,
 }: {
